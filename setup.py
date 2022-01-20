e5fb2285e6bbfcdcf1b7d2e322451de81165c21a
wenbin <wenbinxu@fhi-berlin.mpg.de>
2022-01-20 23:58:53 +0100

    v 0.1.4

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='wwl_model',
+    version = "0.1.3",
+    packages=find_packages()
+)
